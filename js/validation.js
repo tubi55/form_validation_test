5defff919af6960b8e5d2cb5075b6b62fe0020a0
tubi55 <tubi55@nate.com>
2021-11-23 10:55:05 +0900

    step3 email 인증함수

--- js/validation.js
+++ js/validation.js
@@ -2,15 +2,17 @@ var form = document.querySelector("#member");
 var btnSubmit = form.querySelector("input[type=submit]"); 
 
 btnSubmit.addEventListener("click", function(e){
-   if(!isTxt("userid")) e.preventDefault(); 
+   if(!isTxt("userid", 5)) e.preventDefault(); 
+   if(!isTxt("comments", 20)) e.preventDefault();
+   if(!isEmail("email", 5)) e.preventDefault(); 
 }); 
 
 //text 인증함수 
-function isTxt(name){
-   var input = form.querySelector("input[name=${name}]"); 
+function isTxt(name, len){
+   var input = form.querySelector(`[name=${name}]`); 
    var txt = input.value; 
    
-   if(txt !==""){
+   if(txt.length > len){
       var errMsgs = input.closest("td").querySelectorAll("p"); 
       if(errMsgs.length >0) input.closest("td").querySelector("p").remove(); 
 
@@ -20,8 +22,29 @@ function isTxt(name){
       if(errMsgs.length >0) input.closest("td").querySelector("p").remove(); 
 
       var errMsg = document.createElement("p"); 
-      errMsg.append("텍스트를 입력하세요"); 
+      errMsg.append(`텍스트를 ${len}글자 이상 입력하세요`); 
       input.closest("td").append(errMsg); 
+
+      return false; 
+   }
+}
+
+function isEmail(name, len){
+   var input = form.querySelector(`[name=${name}]`); 
+   var txt = input.value; 
+
+   if(txt.length > len && /@/.test(txt)){
+      var errMsgs = input.closest("td").querySelectorAll("p"); 
+      if(errMsgs.length >0) input.closest("td").querySelector("p").remove(); 
+
+      return true; 
+   }else{
+      var errMsgs = input.closest("td").querySelectorAll("p"); 
+      if(errMsgs.length >0) input.closest("td").querySelector("p").remove(); 
+
+      var errMsg = document.createElement("p"); 
+      errMsg.append(`이메일 주소를 ${len}글자 이상 입력해 주세요`); 
+      input.closest("td").append(errMsg);
 
       return false; 
    }
